224e53f93b36e9fc3818c7dffe527660e2bcd67b
sasa-glitch <salahameer200@gmail.com>
2026-04-08 02:08:30 +0200

    small token update

--- src/app/utiles.ts
+++ src/app/utiles.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export async function getDecodedToken(): Promise<string | null> {
     const nextCookies = await cookies();
-    const nextAuthJWTCoded = nextCookies.get("next-auth.session-token")?.value;
+    const nextAuthJWTCoded = nextCookies.get("__Secure-next-auth.session-token")?.value ?? nextCookies.get("next-auth.session-token")?.value;
     const nextAuthJWTDecoded = await decode({token: nextAuthJWTCoded, secret: process.env.NEXTAUTH_SECRET!});
     if (nextAuthJWTDecoded) {
         console.log("This is the token I suppose", nextAuthJWTDecoded)
